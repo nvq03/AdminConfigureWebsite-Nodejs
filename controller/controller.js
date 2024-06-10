421854db926ac83139a2ff1b31a445624ae07158
Chaunghi2508 <145953746+nhocti2106@users.noreply.github.com>
2024-06-10 15:27:00 +0700

    change controller

--- controller/controller.js
+++ controller/controller.js
@@ -62,20 +62,26 @@ exports.getUpdatePage = async (req, res) => {
     res.send("Không tìm thấy người dùng");
   }
 };
-exports.getUpdatePage = async (req, res) => {
-  const userId = req.params.id;
-  const user = await User.findOne({ _id: userId });
 
-  if (user) {
-    res.render("update", { userId: userId, user: user });
-  } else {
-    res.send("Không tìm thấy người dùng");
-  }
-};
 
 
 
 // Hiển thị trang xóa người dùng
+exports.getDeletePage = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findOne({ _id: userId });
+    if (user) {
+      res.render("delete", { userId: userId, user: user });
+    } else {
+      res.send("Người dùng không tồn tại");
+    }
+  } catch (error) {
+    console.error("Lỗi khi xác nhận xóa người dùng:", error);
+    res.send("Đã xảy ra lỗi trong quá trình xác nhận xóa người dùng");
+  }
+};
 
 
 // Tạo người dùng mới
